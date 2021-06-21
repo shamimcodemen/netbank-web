1f3055d730d85ebd7424c2c43f3d8f62c9ede0da
shamim99apk <rianahmedshamim99apk@gmail.com>
2021-06-21 10:35:57 +0600

    set-up initialize

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "royal-blue": "#0741AD",
+        "light-blue": "#2266EA",
+        "darky": "#000000"
+       
+
+
+      },
+      fontFamily: {
+        body: ["Barlow", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
